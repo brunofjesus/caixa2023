defa552bbd2d657ee81bd458d38f3d302dbb493c
Bruno Jesus <bruno.fl.jesus@gmail.com>
2023-03-01 15:17:02 +0000

    Criar printBox

--- src/pt/brunojesus/caixa/Main.java
+++ src/pt/brunojesus/caixa/Main.java
@@ -3,13 +3,25 @@ package pt.brunojesus.caixa;
 public class Main {
 
 	public static void main(String[] args) {
-		printTopOrBottom(6);
-		printLeftAndRight(6,4);
-		printTopOrBottom(6);
+		printBox(4,4);
+		printBox(8,4);
+		printBox(5,5);
 	}
 	
-	public static void printTopOrBottom(int size) {
-		for (int i = 0; i < size; i++) {
+	public static void printBox(int width, int height) {
+		if (width < 4) {
+			width = 4;
+		}
+		if (height < 4) {
+			height = 4;
+		}
+		printTopOrBottom(width);
+		printLeftAndRight(width,height-2);
+		printTopOrBottom(width);
+	}
+	
+	public static void printTopOrBottom(int width) {
+		for (int i = 0; i < width; i++) {
 			System.out.print("*");
 		}
 		System.out.println();
