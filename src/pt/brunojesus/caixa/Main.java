834a9d8476eb6cd01bed0f33832ba201389cf836
Bruno Jesus <bruno.fl.jesus@gmail.com>
2023-03-01 13:56:15 +0000

    First Commit

---
+++ src/pt/brunojesus/caixa/Main.java
@@ -0,0 +1,13 @@
+package pt.brunojesus.caixa;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("******");
+		System.out.println("*    *");
+		System.out.println("*    *");
+		System.out.println("*    *");
+		System.out.println("******");
+	}
+
+}
